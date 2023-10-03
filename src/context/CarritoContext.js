59b51696d37dfd3399949141ea066ea83424672a
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-10-03 10:34:49 -0300

    LocalStorage

--- src/context/CarritoContext.js
+++ src/context/CarritoContext.js
@@ -1,5 +1,4 @@
-import { createContext } from "react";
-import { useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 
 export const CarritoContext = createContext();
 
@@ -11,60 +10,56 @@ const DISMINUIR_CANTIDAD_COMPRA = "[CARRITO] Disminuir Cantidad Compra";
 const ELIMINAR_COMPRA = "[CARRITO] Eliminar Compra";
 
 const CarritoProvider = (props) => {
-  const comprasReducer = (state = initialState, action = {}) => {
+  const almacenarStorageCarrito =
+    JSON.parse(localStorage.getItem("carrito")) || [];
+
+  const comprasReducer = (state, action) => {
     switch (action.type) {
       case AGREGAR_COMPRA:
         return [...state, action.payload];
       case AUMENTAR_CANTIDAD_COMPRA:
-        return state.map((item) => {
-          const cant = item.cantidad + 1;
-          if (item.id === action.payload) return { ...item, cantidad: cant };
-          return item;
-        });
+        return state.map((item) =>
+          item.id === action.payload
+            ? { ...item, cantidad: item.cantidad + 1 }
+            : item
+        );
       case DISMINUIR_CANTIDAD_COMPRA:
-        return state.map((item) => {
-          const cant = item.cantidad - 1;
-          if (item.id === action.payload && item.cantidad > 1)
-            return { ...item, cantidad: cant };
-          return item;
-        });
+        return state.map((item) =>
+          item.id === action.payload && item.cantidad > 1
+            ? { ...item, cantidad: item.cantidad - 1 }
+            : item
+        );
       case ELIMINAR_COMPRA:
-        return state.filter((compra) => compra.id !== action.payload);
+        return state.filter((item) => item.id !== action.payload);
       default:
         return state;
     }
   };
 
-  const [listaCompras, dispatch] = useReducer(comprasReducer, initialState);
+  const [listaCompras, dispatch] = useReducer(
+    comprasReducer,
+    almacenarStorageCarrito
+  );
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(listaCompras));
+  }, [listaCompras]);
 
   const agregarCompra = (compra) => {
     compra.cantidad = 1;
-    const action = {
-      type: AGREGAR_COMPRA,
-      payload: compra,
-    };
-    dispatch(action);
+    dispatch({ type: AGREGAR_COMPRA, payload: compra });
   };
+
   const aumentarCantidad = (id) => {
-    const action = {
-      type: AUMENTAR_CANTIDAD_COMPRA,
-      payload: id,
-    };
-    dispatch(action);
+    dispatch({ type: AUMENTAR_CANTIDAD_COMPRA, payload: id });
   };
+
   const disminuirCantidad = (id) => {
-    const action = {
-      type: DISMINUIR_CANTIDAD_COMPRA,
-      payload: id,
-    };
-    dispatch(action);
+    dispatch({ type: DISMINUIR_CANTIDAD_COMPRA, payload: id });
   };
+
   const eliminarCompra = (id) => {
-    const action = {
-      type: ELIMINAR_COMPRA,
-      payload: id,
-    };
-    dispatch(action);
+    dispatch({ type: ELIMINAR_COMPRA, payload: id });
   };
 
   return (
